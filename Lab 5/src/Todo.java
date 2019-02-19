e159412e31b82e6b06c2971179ccc479c4a2ccbf
Samson Olivero <samsonolivero@gmail.com>
2019-02-19 17:24:50 -0600

    Second Commit added files

---
+++ Lab 5/src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-19-19
+OU ID_______________: 113479362
+OU EMAIL____________: samsonolivero@ou.edu
+
+Tasks:
+1. Fix Calculator Class
+2. J Unit Testing
+
+Estimated Time (minutes):
+1. 120
+2. 60
+
+Actual Time (minutes):
+1. 180
+2. 60
+*/}
